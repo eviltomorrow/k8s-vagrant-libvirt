4fd7f6b5844269fabc7b02c4756c9183fb7fd17d
shepard <eviltomorrow@163.com>
2023-07-12 07:17:14 +0800

    dev

--- sh/common.sh
+++ sh/common.sh
@@ -71,7 +71,7 @@ systemctl restart containerd && systemctl enable --now containerd
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://mirrors.tuna.tsinghua.edu.cn/kubernetes/apt/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
