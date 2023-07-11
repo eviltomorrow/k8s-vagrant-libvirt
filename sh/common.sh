3cd85481f53c0c6239e488e382bf8f6d3a7c1bdd
shepard <eviltomorrow@163.com>
2023-07-12 07:06:46 +0800

    dev

--- sh/common.sh
+++ sh/common.sh
@@ -3,6 +3,15 @@
 echo "Setup base env"
 export DEBIAN_FRONTEND=noninteractive
 
+cat > /etc/apt/sources.list << EOF
+deb http://ftp.cn.debian.org/debian/ bullseye main contrib
+deb http://ftp.cn.debian.org/debian/ bullseye-proposed-updates contrib main
+deb http://ftp.cn.debian.org/debian-security bullseye-security main
+EOF
+
+apt-getupdate
+apt-getupgrade
+
 echo "export PATH=$PATH:/sbin" > ~/.bashrc
 source ~/.bashrc
 
@@ -45,24 +54,24 @@ EOF
 sysctl --system
 
 # install crontainerd.io
-for pkg in docker.io docker-doc docker-compose containerd runc; do sudo apt-get remove $pkg; done
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg vim
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install containerd.io -y
+for pkg in docker.io docker-doc docker-compose containerd runc; do apt-get remove $pkg; done
+apt-get update
+apt-get install -y ca-certificates curl gnupg vim
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install containerd.io -y
 mkdir -p /etc/containerd; cp /vagrant/conf/config.toml /etc/containerd/config.toml
 systemctl restart containerd && systemctl enable --now containerd
 
 # install kubernetes
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
-curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
-sudo systemctl enable kubelet && sudo systemctl restart kubelet
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+systemctl enable kubelet && systemctl restart kubelet
