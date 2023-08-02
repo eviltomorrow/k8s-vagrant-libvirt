aca75c3ce133dbdd5d37810d42d6251e05c5b01a
shepard <eviltomorrow@163.com>
2023-08-02 17:00:58 +0800

    dev

--- sh/bootstrap.sh
+++ sh/bootstrap.sh
@@ -4,10 +4,10 @@ echo "Setup base env"
 export DEBIAN_FRONTEND=noninteractive
 
 cat > /etc/apt/sources.list << EOF
-deb http://ftp.cn.debian.org/debian/ testing main contrib non-free-firmware non-free
-deb http://ftp.cn.debian.org/debian/ testing-proposed-updates non-free-firmware contrib non-free main
-deb http://ftp.cn.debian.org/debian/ testing-updates main non-free-firmware non-free
-deb http://ftp.cn.debian.org/debian/ testing-backports main contrib non-free-firmware non-free
+deb http://ftp.cn.debian.org/debian/ bookworm main contrib non-free-firmware non-free
+deb http://ftp.cn.debian.org/debian/ bookworm-proposed-updates non-free-firmware contrib non-free main
+deb http://ftp.cn.debian.org/debian/ bookworm-updates main non-free-firmware non-free
+deb http://ftp.cn.debian.org/debian/ bookworm-backports main contrib non-free-firmware non-free
 EOF
 
 apt-get update -y
