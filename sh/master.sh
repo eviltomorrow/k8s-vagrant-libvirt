c83761c35834e51d5370d810a46abbe42b3f6cba
shepard <shepard@liarsa.localdomain>
2023-10-10 11:06:07 +0800

    dev

--- sh/master.sh
+++ sh/master.sh
@@ -41,7 +41,7 @@ chmod 0600 /etc/kubernetes/admin.conf
 # kubectl create -f /vagrant/conf/custom-resources.yaml
 
 # 使用 cilium
-mkdir -p /usr/local/app/helm; tar zxvfC /vagrant/bin/helm-v3.12.2-linux-amd64.tar.gz /usr/local/app/helm; ln -s /usr/local/app/helm/linux-amd64/helm /usr/local/bin/helm
+mkdir -p /usr/local/app/helm; tar zxvfC /vagrant/bin/helm-v3.13.0-linux-amd64.tar.gz /usr/local/app/helm; ln -s /usr/local/app/helm/linux-amd64/helm /usr/local/bin/helm
 helm repo add cilium https://helm.cilium.io
 kubectl create namespace cilium-system
 helm install cilium cilium/cilium --namespace cilium-system --set hubble.relay.enabled=true --set hubble.ui.enabled=true --set prometheus.enabled=true --set operator.prometheus.enabled=true --set hubble.enabled=true --set hubble.metrics.enabled="{dns,drop,tcp,flow,port-distribution,icmp,http}"
