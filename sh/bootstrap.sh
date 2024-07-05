d9c0f865612c84e661359d637a78d6025efa9fde
liarsa.localdomain <shepard@liarsa.localdomain>
2024-07-05 12:38:34 +0800

    dev

--- sh/bootstrap.sh
+++ sh/bootstrap.sh
@@ -66,9 +66,13 @@ for pkg in docker.io docker-doc docker-compose containerd runc; do apt-get remov
 apt-get update
 apt-get install -y ca-certificates curl gnupg vim git
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+
+# curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+cat /vagrant/key/docker-ce.gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
-echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 apt-get update
 apt-get install containerd.io -y
 mkdir -p /etc/containerd; cp /vagrant/conf/config.toml /etc/containerd/config.toml
