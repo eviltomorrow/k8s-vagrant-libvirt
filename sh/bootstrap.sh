b04e4925e2e9025a62f62fe6b6a14381a4649180
liarsa.localdomain <shepard@liarsa.localdomain>
2024-07-10 13:18:41 +0800

    dev

--- sh/bootstrap.sh
+++ sh/bootstrap.sh
@@ -68,8 +68,8 @@ apt-get install -y ca-certificates curl gnupg vim git
 install -m 0755 -d /etc/apt/keyrings
 
 
-# curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-cat /vagrant/key/docker-ce.gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# cat /vagrant/key/docker-ce.gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
